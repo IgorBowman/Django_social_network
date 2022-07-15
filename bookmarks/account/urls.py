60ab6eb93bc36044b41bd3a724ebfc73aaee5e07
IgorBowman <xxx@gmaol.com>
2022-07-15 18:36:33 +0500

    [upd] add followers

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -17,10 +17,13 @@ urlpatterns = [
          name='password_reset'),
     path('password_reset/done', auth_views.PasswordResetDoneView.as_view(),
          name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
          name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
          name='password_reset_complete'),
     path('users/', views.user_list, name='user_list'),
+    path('users/follow/', views.user_follow, name='user_follow'),
     path('users/<username>/', views.user_detail, name='user_detail'),
+
 ]
