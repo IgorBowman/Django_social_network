651429559a5914a94adc49b34c5f9f784a8c464b
IgorBowman <jitser26@gmail.com>
2022-07-19 21:59:23 +0500

    [upd] made migrations

--- bookmarks/actions/models.py
+++ bookmarks/actions/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Action(models.Model):
     user = models.ForeignKey('auth.User', related_name='actions',
-                             ad_index=True, on_delete=models.CASCADE)
+                             db_index=True, on_delete=models.CASCADE)
     verb = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True, db_index=True)
     target_ct = models.ForeignKey(ContentType, blank=True,
